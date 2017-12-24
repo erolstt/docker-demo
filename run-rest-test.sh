d949ecb22281f9fc3d611f8867a34dcb10b791c6
erolstt <selitektay@gmail.com>
2017-12-24 13:55:24 +0000

    comment added to run-rest-test.sh

--- run-rest-test.sh
+++ run-rest-test.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+# wait for rest service is available.
+# rest service is defined in docker-compose.test.yml file so it should be accessible to run-rest-test.sh script
 ./wait-for-it.sh -t 60 rest:8080 -- echo "Rest app is ready!"
 ./gradlew rest-service-api-test:test -DBASE_URL=http://rest:8080
